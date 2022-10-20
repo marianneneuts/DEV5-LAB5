d48cfc54ad61af18040b008906e1c2469f4e41fc
Marianne <marianneneuts@gmail.com>
2022-10-20 16:50:02 +0200

    feat: add routes

--- routes/messages.js
+++ routes/messages.js
@@ -1,4 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const messagesController = require('../controllers/messages');
+
+router.get("/", messagesController.getAll);
+router.get("/:id", messagesController.getId);
+router.post("/", messagesController.create);
+router.put("/:id", messagesController.update);
+router.delete("/:id", messagesController.del);
 
 module.exports = router;
